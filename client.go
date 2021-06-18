a77e098582cf0d6d87d97ad90f5f405902e995bf
moeabdol <mohd.a.saed@gmail.com>
2021-06-19 01:41:56 +0300

    Create basic Client struct

--- client.go
+++ client.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
 // Client type
-type Client struct{}
+type Client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/moeabdol/chat-app-golang
 
 go 1.16
+
+require github.com/gorilla/websocket v1.4.2
