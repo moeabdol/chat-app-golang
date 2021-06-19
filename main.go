c0f4250f4ac642dc54c309dfa1d1d0aba6a1508f
moeabdol <mohd.a.saed@gmail.com>
2021-06-19 04:52:30 +0300

    Implement home.html

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "home.html")
+}
+
 func main() {
 	hub := newHub()
 	go hub.run()
 
 	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods(http.MethodGet)
 	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWS(hub, w, r)
 	})
